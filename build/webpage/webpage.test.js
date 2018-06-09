03b577047938db57019e4b8ede0ddfeae5cbb54e
Krzysztof Jamróz <jamro@jamro.net>
2018-06-09 17:34:22 +0200

    removing casperjs gulp plugin that is outdated

--- build/webpage/webpage.test.js
+++ build/webpage/webpage.test.js
@@ -1,23 +1,42 @@
+const through = require("through2");
+const child_process = require('child_process');
+
 module.exports = function (gulp, config, plugins) {
-  return function () {
+  return function() {
+
     plugins.connect.server({
       name: 'Test Server',
       root: config.tmp + 'dist/',
       port: 8070
     });
 
-    const options = {
-      binPath: './node_modules/.bin/casperjs'
-    }
 
     return gulp.src(config.webpage.test)
-      .pipe(plugins.casperjs(options))
+      .pipe(through.obj(function (file, enc, callback) {
+
+        const casperBin = './node_modules/.bin/casperjs';
+
+        let args = [
+          '--log-level=warning',
+          'test',
+          file.path
+        ];
+
+        let child = child_process.execFile(casperBin, args, (error, stdout, stderr) => {
+          this.push(file);
+          callback();
+        });
+
+        child.stdout.on('data', function(data) {
+          plugins.util.log("CasperJS: " + data.toString().replace(/[\n\r]+$/, ""));
+        });
+
+      }))
       .on('error', function(){
         process.emit('exit');
       })
       .on('end', function () {
         plugins.connect.serverClose();
       });
-
-  };
+  }
 };

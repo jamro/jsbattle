05927978ae3f90aab5fe9067c0543cb21b7ca31d
Krzysztof Jamróz <jamro@jamro.net>
2018-06-28 20:13:09 +0200

    parsing hash parameters from URL

--- app/webpage/sources/entry.js
+++ app/webpage/sources/entry.js
@@ -1,10 +1,20 @@
 import 'babel-polyfill';
 import App from "./components/App.js";
 
-let stateless = false;
-if(window.location.hash) {
-  stateless = window.location.hash.substring(1) == 'stateless';
+function getHashParameters() {
+  let serial = window.location.hash ? window.location.hash.substring(1) : "";
+  let params = serial.split("&");
+  let result = [];
+  for(let keyValue of params) {
+    keyValue = keyValue.split("=", 2);
+    result[keyValue[0]] = keyValue[1] ? keyValue[1] : null;
+  }
+
+  return result;
 }
+
+let params = getHashParameters();
+let stateless = (params.stateless !== undefined && params.stateless != false);
 
 ReactDOM.render(
   <App renderer="brody" stateless={stateless} />,

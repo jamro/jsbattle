7532400c4c9d160e57ca595f3f3b25e60c3c8143
Krzysztof Jamroz <jamro@jamro.net>
2019-03-13 14:01:43 +0100

    build: add watch mode for all packages

--- packages/jsbattle-server/src/index.js
+++ packages/jsbattle-server/src/index.js
@@ -1,2 +1,2 @@
-exports.Gateway = require('./services/Gateway.js')
-exports.UbdArenaService = require('./lib/UbdArenaService.js')
+exports.Gateway = require('./services/Gateway.js');
+exports.UbdArenaService = require('./lib/UbdArenaService.js');

cd9a32cd078a3daf64edc71ba24b10fa941f019b
Krzysztof Jamroz <jamro@jamro.net>
2020-07-07 19:41:43 +0200

    build(jsbattle): add start-dev npm scripts to all packages

--- packages/jsbattle-mockserver/app/cli.js
+++ packages/jsbattle-mockserver/app/cli.js
@@ -5,16 +5,21 @@ let server = new MockServer();
 
 console.log(process.argv);
 let port = 8080;
+let publicPath = './dist';
 for(let i=0; i < process.argv.length; i++) {
   if(process.argv[i] == '--port') {
     i++
     port = process.argv[i];
   }
+  if(process.argv[i] == '--www') {
+    i++
+    publicPath = process.argv[i];
+  }
 }
 
 server.start({
   port: port,
-  public: './dist',
+  public: publicPath,
   authorized: false,
   serverDelay: 100,
   rootUrl: '/'
